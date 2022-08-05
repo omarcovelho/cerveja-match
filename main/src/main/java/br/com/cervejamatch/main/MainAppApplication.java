5c1f0f81598073fbcbe86c14e9a559d72e09d3d4
Marco Prado <marco.asprado91@gmail.com>
2022-08-04 23:17:49 -0300

    Add Main App

---
+++ main/src/main/java/br/com/cervejamatch/main/MainAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.cervejamatch.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppApplication.class, args);
+	}
+
+}
